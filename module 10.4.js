e31e0773476b983202709f9a50c11a3739c47484
Aleksey1Nekrasov <a.s.nekrasov69@mail.ru>
2024-10-27 21:59:15 +0300

    first commit

---
+++ module 10.4.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  let x = window.screen.width;
+  let y = window.screen.height;
+  alert("Ширина экрана: " +x+ " Высота экрана: " +y) 
+  });
